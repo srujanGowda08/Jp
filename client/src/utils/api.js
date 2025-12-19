c4954da8c4286cc0f02eb97bba637a0c721f58d5
Hemanth-diatoz <hemanth.ir@diatoz.com>
2025-12-19 22:26:51 +0530

    final

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://jp-e761exoo8-srujan-s-projects.vercel.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
