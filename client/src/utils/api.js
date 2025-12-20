aa2077a67226619e8123275c7bd470e975c616cc
Hemanth-diatoz <hemanth.ir@diatoz.com>
2025-12-20 18:54:01 +0530

    latest

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://jp-e761exoo8-srujan-s-projects.vercel.app/api',
+    baseURL: 'https://jp-v6f5.vercel.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
