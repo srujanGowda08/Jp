680ad75a878281901b7a1d8245d11bc5c1ef6871
Srujan G S <148607148+srujanGowda08@users.noreply.github.com>
2025-12-20 18:59:32 +0530

    Update API base URL to new endpoint Added
    Backend Deployed API

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://jp-v6f5.vercel.app/api',
+    baseURL: 'https://jp-delta.vercel.app/api',
     headers: {
         'Content-Type': 'application/json'
     }
